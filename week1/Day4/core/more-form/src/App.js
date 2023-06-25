a30cf2cc25d7476710df740ad43cf85082536732
BENAMORNAWFAL <technobenamor@gmail.com>
2023-06-25 19:49:06 +0100

    validation form react

---
+++ week1/Day4/core/more-form/src/App.js
@@ -0,0 +1,16 @@
+import Formreact from "./components/Formreact"
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Formreact/>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
